7de4b277baa27e4d237c9014f877b4f95fdc1bd6
jinligithub <1018487750@qq.com>
2019-04-28 22:00:54 +0800

    字符串匹配题

--- 牛客/字符串/乒乓球框.java
+++ 牛客/字符串/乒乓球框.java
@@ -52,3 +52,39 @@ public class Main{
         }
     }
 }
+
+import java.util.*;
+public class Main {
+   public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        while(read.hasNextLine()) {
+            String r = read.nextLine();
+            String[] boxs = r.split(" ");
+            ArrayList<Character> A = new ArrayList<Character>();
+            ArrayList<Character> B = new ArrayList<Character>();
+            for(int i = 0; i < boxs[0].length(); i++) {
+                A.add(boxs[0].charAt(i));
+            }
+            for(int i = 0; i < boxs[1].length(); i++) {
+                B.add(boxs[1].charAt(i));
+            }
+            for(int i = 0; i < B.size(); i++) {
+                if(A.contains(B.get(i))) {
+				    //get(int index) 返回此列表中指定位置的元素。  
+                    //indexOf(Object o) 返回此列表中指定元素的第一次出现的索引，如果此列表不包含元素，则返回-1。					
+                    A.remove(A.get(A.indexOf(B.get(i))));
+                    B.remove(i);
+					//保证他都是从B箱子里第一个元素开始的
+                    i--;
+                }
+            }
+            if(B.size() == 0) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+            A = null;
+            B = null;
+        }
+    }
+}
