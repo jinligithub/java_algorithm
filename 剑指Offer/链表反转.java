3d3900c42365ab8c62c6e8063822109bc23ad3a0
jinligithub <1018487750@qq.com>
2019-08-22 20:08:20 +0800

    补充

--- 剑指Offer/链表反转.java
+++ 剑指Offer/链表反转.java
@@ -35,3 +35,27 @@ public class Solution {
         return pre;
     }
 }
+
+补充：
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head==null||head.next==null)//如果只有一个节点直接返回节点
+            return head;
+        ListNode temp=null;//创建一个新的链表
+        while(head!=null){
+            ListNode next=head.next;//保存head后边的链表
+            head.next=temp;//改变头结点的指向
+            temp=head;//让temp的节点为新链表的头结点,每次插入一个元素后新插入发的元素就是新的头结点，这样最后进来的元素就是新的头结点，然后向后输出元素，就是链表的逆序
+            head=next;//继续遍历head后边的节点
+        }
+        return temp;
+    }
+}
